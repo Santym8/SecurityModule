ca467bc07bce80c1c949647e8eb66dcc9d9e6864
¨Santym¨ <santimarcelofierrod@gmail.com>
2023-12-03 18:17:07 -0500

    Add API configuration and endpoints

--- SecurityModule/settings.py
+++ SecurityModule/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",  # Django REST framework
 ]
 
 MIDDLEWARE = [
